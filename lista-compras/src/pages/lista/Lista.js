e77b0bd60499f9ce0cd6bcd0032ad8f76970f169
maicom <maicom.lithiun@yahoo.com.br>
2019-09-25 22:41:33 -0400

    retorno de lista

--- lista-compras/src/pages/lista/Lista.js
+++ lista-compras/src/pages/lista/Lista.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import Menu from '../../components/menu/Menu';
 import anterior from '../../img/anterior.png';
+import ListaService from '../../services/ListaService'
 class Lista extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            lista: this.props.location.state.lista
+            lista: this.props.location.state.lista,
+            itensFiltrados :[]
         };
+        this.service = new ListaService();
+    }
+    /**
+     * aplica o filtro do usuário nos
+     * itens de lista
+     */
+    filtrarItens = (event) => {
+        //pega o valor digitado do campo de filtro
+        let filtro = event.target.value;
+        let itensFiltrados = this.service.recuperarItens(filtro);
+        console.log(itensFiltrados);
     }
     render() {
+        let { lista } = this.state;
         return (
             <div>
                 <Menu
@@ -16,7 +30,11 @@ class Lista extends Component {
                     paginaAnterior="/"
 
                     titulo="Minha lista" />
-                Lista
+                <h3 id="nomeLista">{lista.nome}</h3>
+                <form>
+                    <input onChange={this.filtrarItens} name="filtro"
+                        type="text/" />
+                </form>
             </div>
         );
     }
--- lista-compras/src/services/ListaService.js
+++ lista-compras/src/services/ListaService.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 
 import itens from './itens';
+
+const LIMITE_DE_ITENS = 5;
+
 export default class ListaService {
     constructor() {
         this.listas = [];
         this.api = axios.create({
-            //baseURL: 'http://localhost:3001/listas'
-            baseURL: 'https://secret-plateau-95576.herokuapp.com/listas'
+            baseURL: 'http://localhost:3001/listas'
+            //baseURL: 'https://secret-plateau-95576.herokuapp.com/listas'
         });
     }
     async recuperarListas() {
@@ -22,15 +25,24 @@ export default class ListaService {
     }
 
     recuperarItens(termo) {
+        /*senão houiver nem um termo ,a função
+         retornara uma lista vazia */
+        if(!termo){
+            return[];
+        }
         //Converte para minusculo
-
         termo = termo.toLowerCase();
         //Função de filtragem
-        return itens.filter(item => {
+    
+        let itensFiltrados= itens.filter(item => {
             let aux = item.descricao.toLowerCase();
             let descricao = aux;
             return descricao.includes(termo);
         });
+        if (itensFiltrados.length > LIMITE_DE_ITENS){
+            return itensFiltrados.slice(0,LIMITE_DE_ITENS);
+        }
+        return itensFiltrados;
 
     }
 }
